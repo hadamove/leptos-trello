056ba14eddfb47534d601b35942546dc50f42435
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2023-08-02 20:11:30 +0200

    make cards draggable

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use leptos::html::Div;
 use leptos::*;
+use leptos_use::*;
 
 struct Card {
     name: String,
@@ -7,8 +9,28 @@ struct Card {
 
 #[component]
 fn CardView(cx: Scope, card: Card) -> impl IntoView {
+    let el = create_node_ref::<Div>(cx);
+    let (is_dragging, set_is_dragging) = create_signal(cx, false);
+
+    let UseDraggableReturn { style, .. } = use_draggable_with_options(
+        cx,
+        el,
+        UseDraggableOptions::default()
+            .prevent_default(true)
+            .on_move(move |_| {
+                set_is_dragging(true);
+            })
+            .on_end(move |_| {
+                set_is_dragging(false);
+            }),
+    );
+
     view! { cx,
-        <div class="bg-white rounded p-4 mb-4">
+        <div
+            node_ref=el
+            class=move || format!("bg-white rounded p-4 mb-4 {}", if is_dragging.get() { "shadow-lg pointer-events-none max-w-xs" } else { "" })
+            style=move || if is_dragging.get() { format!("position: fixed; {}", style.get()) } else {"".to_owned()}
+        >
             <h2 class="text-lg font-bold">{card.name}</h2>
             <p class="text-gray-600">{card.description}</p>
         </div>
@@ -17,13 +39,18 @@ fn CardView(cx: Scope, card: Card) -> impl IntoView {
 
 #[component]
 fn CardList(cx: Scope, title: String, cards: Vec<Card>) -> impl IntoView {
+    let el = create_node_ref::<Div>(cx);
+    let is_hovered = use_element_hover(cx, el);
+
     view! { cx,
-        <div class="flex-1 max-w-sm bg-gray-100 rounded p-4">
+        <div class="flex-1 max-w-sm bg-gray-100 rounded p-4" node_ref=el>
             <h1 class="text-xl font-bold mb-4 text-blue-600">{title}</h1>
             {cards
                 .into_iter()
                 .map(|card| view! { cx, <CardView card/> })
                 .collect_view(cx)}
+
+            { move || is_hovered.get().then(|| view! { cx, <button class="bg-blue-600 text-white rounded p-2">Add</button> }) }
         </div>
     }
 }
@@ -33,7 +60,7 @@ fn App(cx: Scope) -> impl IntoView {
     let todo = vec![
         Card {
             name: "Groceries".to_string(),
-            description: "Buy groceries for the week".to_string(),
+            description: "Buy groceries for the week and also some other goodies".to_string(),
         },
         Card {
             name: "Laundry".to_string(),
