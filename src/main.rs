159fa7688f1f8b17031d023c6dc2557334b50bcc
Hugo Adamove <55924102+hadamove@users.noreply.github.com>
2023-08-03 17:19:43 +0200

    card delition WIP

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use leptos::html::Div;
+use leptos::leptos_dom::console_log;
 use leptos::*;
 use leptos_use::*;
 
@@ -35,6 +36,8 @@ fn CardView(cx: Scope, card: Card) -> impl IntoView {
             }),
     );
 
+    let CardsContext { set_cards, .. } = use_context::<CardsContext>(cx).expect("Cards not found");
+
     view! { cx,
         <div
             node_ref=el
@@ -45,28 +48,58 @@ fn CardView(cx: Scope, card: Card) -> impl IntoView {
         >
             <h2 class="text-lg font-bold">{card.name}</h2>
             <p class="text-gray-600">{card.description}</p>
+
+            <div class="flex justify-end gap-2">
+                // Edit
+                <button
+                    class="bg-blue-100 rounded p-1 mt-2"
+                >
+                    "️✏️"
+                </button>
+
+                // Delete
+                <button
+                    class="bg-red-100 rounded p-1 mt-2"
+                    on:click=move |_| {
+                        console_log("Deleting card");
+                        set_cards.update(move |cards| {
+                            cards.retain(|c| c.id != card.id);
+                        });
+                    }
+                >
+                    "️🔥"
+                </button>
+            </div>
         </div>
     }
 }
 
 #[component]
-fn CardList(cx: Scope, card_state: CardState, cards: Vec<Card>) -> impl IntoView {
+fn CardList(cx: Scope, card_state: CardState) -> impl IntoView {
     let el = create_node_ref::<Div>(cx);
     let is_hovered = use_element_hover(cx, el);
+    let CardsContext { cards, .. } = use_context::<CardsContext>(cx).expect("Cards not found");
 
     view! { cx,
         <div class="flex-1 max-w-sm bg-gray-100 rounded p-4" node_ref=el>
             <h1 class="text-xl font-bold mb-4 text-blue-600">
                 {format!("{:?}", card_state)}
             </h1>
-            {cards
+            {move || cards.get()
                 .into_iter()
+                .filter(|card| card.state == card_state)
                 .map(|card| view! { cx, <CardView card/> })
                 .collect_view(cx)}
 
             { move || is_hovered.get().then(|| view! { cx, <button class="bg-blue-600 text-white rounded p-2">Add</button> }) }
         </div>
     }
+}
+
+#[derive(Clone)]
+struct CardsContext {
+    cards: ReadSignal<Vec<Card>>,
+    set_cards: WriteSignal<Vec<Card>>,
 }
 
 #[component]
@@ -118,20 +151,26 @@ fn App(cx: Scope) -> impl IntoView {
             },
         ],
     );
+    provide_context(cx, CardsContext { cards, set_cards });
+
+    // let add_card = move |card: Card| {
+    //     let mut cards = cards.get();
+    //     cards.push(card);
+    //     set_cards(cards);
+    // };
+
+    // let update_card = move |id: usize, changed_card: Card| {
+    //     let mut cards = cards.get();
+    //     let card = cards.iter_mut().find(|card| card.id == id).unwrap();
+    //     *card = changed_card;
+    //     set_cards(cards);
+    // };
 
     view! { cx,
         <div class="container mx-auto px-4 py-8">
             <div class="flex gap-4">
-                {[CardState::Todo, CardState::InProgress, CardState::Done]
-                    .into_iter()
-                    .map(|state|
-                        view! { cx,
-                            <CardList
-                                card_state=state
-                                cards={cards.get().into_iter().filter(|card| card.state == state).collect()}
-                            />
-                        }
-                    )
+                {[CardState::Todo, CardState::InProgress, CardState::Done].into_iter()
+                    .map(|state| view! { cx, <CardList card_state=state/> })
                     .collect_view(cx)}
             </div>
         </div>
